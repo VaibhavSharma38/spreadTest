9fe47ba789ac612ffd8b761c2cbf7127d6868f3b
Vaibhav Sharma <vaibhavsharma38@gmail.com>
2019-07-28 12:56:47 +0530

    Added mongo module for node and initialize basic code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console,log('Hello World!');
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+
+const words = require('./dictionary');
+
+const crypto = require('crypto');
+const secret = 'abcdefg';
+
+// const hash = crypto.createHmac('sha256', secret)
+//                    .update('I love cupcakes')
+//                    .digest('hex');
+
+mongo.connect(url, function (err, client) {
+	if (err) {
+    	console.error(err);
+    	return;
+  	}
+
+  	const db = client.db('spreadTest');
+  	const collection = db.collection('wordHash');
+
+
+
+  	// collection.insertOne({}, function(err, response){
+  	// 	if(err){
+  	// 		console.log(err);
+  	// 	}
+  	// });
+});
